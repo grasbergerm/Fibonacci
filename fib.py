34dd824ad0d8b2ed365d13f2c3ef680436114646
Matt Grasberger <grasbergerm@gmail.com>
2016-02-09 14:32:13 -0500

    First but completed commit

---
+++ fib.py
@@ -0,0 +1,33 @@
+def slow_fib(number,sum):
+	# base cases
+	if number == 0:
+		return 0
+	elif number == 1:
+		return 1
+	# recursively call fib, very inefficient because
+	# each recursive call ends up making another
+	# recursive call, so we always make 2^n calls
+	else:
+		return slow_fib(number-2) + slow_fib(number-1)
+
+def fib_sum(number):
+	a = 0
+	b = 1
+	# iteratively sum up the fibonacci numbers
+	for i in range(number-2):
+		a, b = b, a + b
+	return a + b
+
+def fast_fib(n):
+	# call fib_helper with the base cases
+	return fib_helper(0,1,n)
+
+def fib_helper(a,b,n):
+	# when 'iteration' (read recursion) is finished
+	# return a to get the answer
+	if n == 0:
+		return a
+	# else call fib_helper with arguments b and sum of a and b
+	# essentially 'looping' while calculation the fibonacci sequence
+	else:
+		return fib_helper(b,(a+b),(n-1))
